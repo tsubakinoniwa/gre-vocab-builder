a1cc7fe57ab077a6fc1d115fc2f37af860b12425
Junjiang Li <lij111@miamioh.edu>
2020-05-19 18:26:29 -0400

    Project Init

---
+++ GREVocab/GREVocab/Record.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace GREVocab {
+    /*
+     * Class representing a row of record in the database.
+     * Holds an Json string that will expand to a Word object.
+     */
+    [Table("record")]
+    public class Record {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        public string Json { get; set; }
+        public DateTime LastMemorized { get; set; }
+    }
+}
---
+++ GREVocab/GREVocab/Word.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace GREVocab {
+    /*
+     * Word class holding all the necessary attributes that 
+     * the Json string in Record will expand into
+     */
+    public class Word {
+        [JsonProperty("word")]
+        public string Content;
+        [JsonProperty("part_of_speech")]
+        public string PartOfSpeech;
+        [JsonProperty("definition_EN")]
+        public string DefinitionEN;
+        [JsonProperty("definition_CN")]
+        public string DefinitionCN;
+        [JsonProperty("synonym")]
+        public string[] Synonyms;
+        [JsonProperty("gre_synonym")]
+        public string[] GRESynonyms;
+    }
+}
