4c4d24fb78e2c093bfbd29280c4b4f39f86ecc24
Junjiang Li <lij111@miamioh.edu>
2020-05-19 18:30:40 -0400

    Creating Init Structure

---
+++ GREVocab/GREVocab/OverviewPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GREVocab {
+    public partial class OverviewPage : ContentPage {
+        public OverviewPage() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GREVocab/GREVocab/SettingsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GREVocab {
+    public partial class SettingsPage : ContentPage {
+        public SettingsPage() {
+            InitializeComponent();
+        }
+    }
+}
