fe4466cdee27afabe8cabc9e0991c9e82a791e75
Ruoxin Li <lir25@miamioh.edu>
2020-05-22 10:34:49 +0800

    add android version

---
+++ GREVocab/clean.bat
@@ -0,0 +1,9 @@
+rmdir GREVocab\obj /S /Q
+rmdir GREVocab\bin /S /Q
+rmdir GREVocab.Android\obj /S /Q
+rmdir GREVocab.Android\bin /S /Q
+rmdir GREVocab.iOS\obj /S /Q
+rmdir GREVocab.iOS\bin /S /Q
+
+rmdir .vs /S /Q
+
